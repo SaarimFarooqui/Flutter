667144ef7b10a573bc85ebaa1882b6bab6ee28cd
Saarim Farooqui <saarimfarooqui000@gmail.com>
2024-11-08 23:43:59 +0500

    Meal selection app initial commit

---
+++ meal_selection_app/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          color: const Color.fromARGB(255, 0, 34, 50)
+        ),
+      ),
+    );
+  }
+}
