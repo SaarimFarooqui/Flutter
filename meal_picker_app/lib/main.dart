47c3d3dc8d1460db43490699b22d5b2d1a032891
Saarim Farooqui <saarimfarooqui000@gmail.com>
2024-11-12 09:28:52 +0500

    meal containers on home page

--- meal_picker_app/lib/main.dart
+++ meal_picker_app/lib/main.dart
@@ -37,7 +37,31 @@ class _homepageState extends State<homepage> {
         decoration: BoxDecoration(
           gradient: LinearGradient(colors: [Colors.green, Colors.greenAccent])
         ),
-        
+        child: ListView(
+          children: [ 
+            listbox("Burger", "PKR 100"),
+            listbox("Pizza", "PKR 200"),
+            listbox("Juice", "PKR 80")
+          ],
+        ),
+      ),
+    );
+  }
+  Container listbox(String mealName, String mealPrice){
+    return Container(
+      margin: EdgeInsets.fromLTRB(5, 10, 5, 0),
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 2),
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white 
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(mealName, style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800),),
+          Text(mealPrice, style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800),)
+        ],
       ),
     );
   }
